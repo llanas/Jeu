78e877c03a46f0e2aeecef2c9d7cce40052d765d
Boris MAURENCE <b.maurence@gmail.com>
2016-12-15 18:37:29 +0100

    Modification du générateur de chemin, amélioration de l'algo en cours.

--- Labyrinthe/src/dao/labyrinthe/LabyrintheDaoModelPremier.java
+++ Labyrinthe/src/dao/labyrinthe/LabyrintheDaoModelPremier.java
@@ -15,43 +15,56 @@ public class LabyrintheDaoModelPremier implements LabyrintheDao {
 	public void generateurChemin(Labyrinthe labyrinthe) {
 
 		initialiserChemin(labyrinthe);
-		int tailleLab = labyrinthe.getTaille()*labyrinthe.getTaille();
-		System.out.println(chemins.get(0).size());
-		while(chemins.get(0).size() != tailleLab) {
+		List<Integer> portesPossible = new ArrayList<>(labyrinthe.getTaille());
+		while(!portesPossible.isEmpty()) {
 			Random rand = new Random();
-			int x = rand.nextInt(chemins.size()-1);
-			System.out.println("Case à traiter    :               " + x);
-			Case caseX = labyrinthe.getCase(x);
-			System.out.println("VALEUR            :               " + caseX.getNumero());
-			int y = 0;
-			int test = rand.nextInt(2);
-			if( test == 0 && caseX.getColonne() != labyrinthe.getTaille()-1 ) {
-				y = x + labyrinthe.getTaille();
-				Case caseY = labyrinthe.getCase(y);
-				System.out.println("Case à comparer :                 " + caseY.getPosition());
-				System.out.println("VALEUR          :                 " + caseY.getNumero());
-				if(caseX.getNumero() != caseY.getNumero()) {
-					comparerCase(caseX, caseY);
-					caseX.setB();
+			int porteATraiter = rand.nextInt(portesPossible.size());
+			System.out.println("Porte à traiter    :               " + porteATraiter);
+			int positionCaseX = (int) Math.floor(porteATraiter/2);
+			Case caseX = labyrinthe.getCase(positionCaseX);
+			System.out.println("VALEUR             :               " + caseX.getNumero());
+			int choixPorteCase = ( porteATraiter % 2);
+			if(choixPorteCase==0) {
+				//PORTE BASSE A OUVRIR
+				Case caseY = labyrinthe.getCase( positionCaseX + labyrinthe.getTaille() );
+				if( caseX.getNumero() < caseY.getNumero() ) {
+					caseY.setNumero(caseX.getNumero());
+				} else {
+					caseX.setNumero(caseY.getNumero());
 				}
-				
-			} else if (test == 1 && caseX.getLigne() != labyrinthe.getTaille()-1 ) {
-				y = x + 1;
-				Case caseY = labyrinthe.getCase(y);
-				System.out.println("Case à comparer :                 " + caseY.getPosition());
-				System.out.println("VALEUR          :                 " + caseY.getNumero());
-				if(caseX.getNumero() != caseY.getNumero()) {
-					comparerCase(caseX, caseY);
-					caseX.setD();
+				caseX.setB();
+			} else {
+				//PORTE DROITE A OUVRIR
+				Case caseY = labyrinthe.getCase( positionCaseX + 1 );
+				if( caseX.getNumero() < caseY.getNumero() ) {
+					caseY.setNumero(caseX.getNumero());
+				} else {
+					caseX.setNumero(caseY.getNumero());
 				}
+				caseX.setD();
 			}
-			chemins.forEach(c ->{
-				c.forEach(l -> {
-					System.out.print(l.getPosition() + "-");
-				});
-				System.out.print("\n");
-			});
-			
+//			int y = 0;
+//			int test = rand.nextInt(2);
+//			if( test == 0 && caseX.getColonne() != labyrinthe.getTaille()-1 ) {
+//				y = x + labyrinthe.getTaille();
+//				Case caseY = labyrinthe.getCase(y);
+//				System.out.println("Case à comparer :                 " + caseY.getPosition());
+//				System.out.println("VALEUR          :                 " + caseY.getNumero());
+//				if(caseX.getNumero() != caseY.getNumero()) {
+//					comparerCase(caseX, caseY);
+//					caseX.setB();
+//				}
+//				
+//			} else if (test == 1 && caseX.getLigne() != labyrinthe.getTaille()-1 ) {
+//				y = x + 1;
+//				Case caseY = labyrinthe.getCase(y);
+//				System.out.println("Case à comparer :                 " + caseY.getPosition());
+//				System.out.println("VALEUR          :                 " + caseY.getNumero());
+//				if(caseX.getNumero() != caseY.getNumero()) {
+//					comparerCase(caseX, caseY);
+//					caseX.setD();
+//				}
+//			}
 		}
 	}
 	
@@ -78,5 +91,29 @@ public class LabyrintheDaoModelPremier implements LabyrintheDao {
 			chemins.remove(x.getPosition());
 		}
 	}
+	
+	/*
+	 * NOUVEAU MODEL:
+	 * 
+	 * int T = taille; //largeur du labyrinthe;
+	 * 
+	 * private List<Integer> portePossible = new ArrayList<Integer>(2T²);
+	 * 
+	 * Random rand = new Random();
+	 * 
+	 * int porteATraiter = rand.nextInt(portePossible.length());
+	 * 
+	 * Case caseATraiter = labyrinthe.getCase(Math.floor(porteATraiter)) //Arondie inférieur
+	 * 
+	 * if(porteATraiter % 2 = 0 ) {
+	 * 		traiterPorteBasse(caseATraiter);
+	 * } else {
+	 * 		traiterPorteDroite(caseATraiter);
+	 * }
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
 
 }
